e22603cd7898dcb0d6a9342a40f8fbf61644edc4
berj <berj@0aca7f79-8821-0410-92b0-ef0cb72988e5>
2003-02-28 23:13:58 +0000

    initial release of a subsystem to encapsulate functionality differences between different renderman renderers

---
+++ trunk/src/liqAqsisRenderer.cpp
@@ -0,0 +1,73 @@
+/*
+**
+** The contents of this file are subject to the Mozilla Public License Version
+** 1.1 (the "License"); you may not use this file except in compliance with
+** the License. You may obtain a copy of the License at
+** http://www.mozilla.org/MPL/ 
+** 
+** Software distributed under the License is distributed on an "AS IS" basis,
+** WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+** for the specific language governing rights and limitations under the
+** License. 
+**
+** The Original Code is the Liquid Rendering Toolkit. 
+** 
+** The Initial Developer of the Original Code is Colin Doncaster. Portions
+** created by Colin Doncaster are Copyright (C) 2002. All Rights Reserved. 
+** 
+** The RenderMan (R) Interface Procedures and Protocol are: Copyright 1988,
+** 1989, Pixar All Rights Reserved
+**
+**
+** RenderMan (R) is a registered trademark of Pixar
+*/
+
+#include <liqAqsisRenderer.h>
+
+bool
+liqAqsisRenderer::supports(e_capability capability) const
+{
+    bool supported = false;
+
+    // TODO: unsure of these for aqsis
+
+    switch(capability)
+    {
+	case BLOBBIES:
+	    supported = true;
+	    break;
+
+	case POINTS:
+	    supported = true;
+	    break;
+
+	case EYESPLITS:
+	    supported = true;
+	    break;
+
+	// no default case. let the compiler catch it if we're missing
+	// something
+    }
+
+    return supported;
+}
+
+bool
+liqAqsisRenderer::requires(e_requirement requirement) const
+{
+    bool required = false;
+
+    // TODO: unsure of these for aqsis
+
+    switch(requirement)
+    {
+	case SWAPPED_UVS:
+	    required = false;
+	    break;
+	
+	// no default case. let the compiler catch it if we're missing
+	// something
+    }
+
+    return required;
+}
