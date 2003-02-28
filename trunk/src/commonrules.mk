3de9322df5e1b451496af8a0bc9c6d7be2798e04
chapman <chapman@0aca7f79-8821-0410-92b0-ef0cb72988e5>
2003-02-28 17:47:05 +0000

    added liqWriteArchive command

--- trunk/src/commonrules.mk
+++ trunk/src/commonrules.mk
@@ -27,62 +27,64 @@ LIQUIDBINLIBS    =  $(LIQRMANLIBS) $(MAYALIBS) \
           -lCommandEngine \
           -lFoundation \
           -lStringCatalog \
-	  $(EXTRA_LIBS) \
-	  -lm
+          $(EXTRA_LIBS) \
+          -lm
 
-LIQUIDMAINOBJS =    liqShader.$(OBJEXT) \
-    	    	    liqRibParticleData.$(OBJEXT) \
-    	    	    liqTokenPointer.$(OBJEXT) \
-    	    	    liquidRibObj.$(OBJEXT) \
-    	    	    liquidRibNode.$(OBJEXT) \
-    	    	    liquidRibData.$(OBJEXT) \
-    	    	    liquidGlobalHelpers.$(OBJEXT) \
-    	    	    liquidRibSurfaceData.$(OBJEXT) \
-    	    	    liquidRibNuCurveData.$(OBJEXT) \
-    	    	    liquidRibMeshData.$(OBJEXT) \
-    	    	    liquidRibLocatorData.$(OBJEXT) \
-    	    	    liquidRibCoordData.$(OBJEXT) \
-    	    	    liquidRibLightData.$(OBJEXT) \
-    	    	    liquidRibHT.$(OBJEXT) \
-    	    	    liquidRibTranslator.$(OBJEXT) \
-    	    	    liquidGetSloInfo.$(OBJEXT) \
-    	    	    liquidGetAttr.$(OBJEXT) \
-    	    	    liquidAttachPrefAttribute.$(OBJEXT) \
-    	    	    liquidRibGenData.$(OBJEXT) \
-    	    	    liqPreviewShader.$(OBJEXT) \
-    	    	    liquidRibSubdivisionData.$(OBJEXT) \
-    	    	    liquidMemory.$(OBJEXT) \
-    	    	    liquidProcessLauncher.$(OBJEXT)
+LIQUIDMAINOBJS =	liqShader.$(OBJEXT) \
+					liqRibParticleData.$(OBJEXT) \
+					liqTokenPointer.$(OBJEXT) \
+					liquidRibObj.$(OBJEXT) \
+					liquidRibNode.$(OBJEXT) \
+					liquidRibData.$(OBJEXT) \
+					liquidGlobalHelpers.$(OBJEXT) \
+					liquidRibSurfaceData.$(OBJEXT) \
+					liquidRibNuCurveData.$(OBJEXT) \
+					liquidRibMeshData.$(OBJEXT) \
+					liquidRibLocatorData.$(OBJEXT) \
+					liquidRibCoordData.$(OBJEXT) \
+					liquidRibLightData.$(OBJEXT) \
+					liquidRibHT.$(OBJEXT) \
+					liquidRibTranslator.$(OBJEXT) \
+					liquidGetSloInfo.$(OBJEXT) \
+					liquidGetAttr.$(OBJEXT) \
+					liquidAttachPrefAttribute.$(OBJEXT) \
+					liquidRibGenData.$(OBJEXT) \
+					liqPreviewShader.$(OBJEXT) \
+					liquidRibSubdivisionData.$(OBJEXT) \
+					liquidMemory.$(OBJEXT) \
+					liquidProcessLauncher.$(OBJEXT) \
+					liqWriteArchive.$(OBJEXT)
 
-LIQUIDOUTMAINOBJS = $(VPATH)/liqShader.$(OBJEXT) \
-    	    	    $(VPATH)/liqRibParticleData.$(OBJEXT) \
-    	    	    $(VPATH)/liqTokenPointer.$(OBJEXT) \
-    	    	    $(VPATH)/liquidRibObj.$(OBJEXT) \
-    	    	    $(VPATH)/liquidRibNode.$(OBJEXT) \
-    	    	    $(VPATH)/liquidRibData.$(OBJEXT) \
-    	    	    $(VPATH)/liquidGlobalHelpers.$(OBJEXT) \
-    	    	    $(VPATH)/liquidRibSurfaceData.$(OBJEXT) \
-    	    	    $(VPATH)/liquidRibNuCurveData.$(OBJEXT) \
-    	    	    $(VPATH)/liquidRibMeshData.$(OBJEXT) \
-    	    	    $(VPATH)/liquidRibLocatorData.$(OBJEXT) \
-    	    	    $(VPATH)/liquidRibCoordData.$(OBJEXT) \
-    	    	    $(VPATH)/liquidRibLightData.$(OBJEXT) \
-    	    	    $(VPATH)/liquidRibHT.$(OBJEXT) \
-    	    	    $(VPATH)/liquidRibTranslator.$(OBJEXT) \
-    	    	    $(VPATH)/liquidGetSloInfo.$(OBJEXT) \
-    	    	    $(VPATH)/liquidGetAttr.$(OBJEXT) \
-    	    	    $(VPATH)/liquidAttachPrefAttribute.$(OBJEXT) \
-    	    	    $(VPATH)/liquidRibGenData.$(OBJEXT) \
-    	    	    $(VPATH)/liqPreviewShader.$(OBJEXT) \
-    	    	    $(VPATH)/liquidRibSubdivisionData.$(OBJEXT) \
-    	    	    $(VPATH)/liquidMemory.$(OBJEXT) \
-    	    	    $(VPATH)/liquidProcessLauncher.$(OBJEXT)
+LIQUIDOUTMAINOBJS =	$(VPATH)/liqShader.$(OBJEXT) \
+					$(VPATH)/liqRibParticleData.$(OBJEXT) \
+					$(VPATH)/liqTokenPointer.$(OBJEXT) \
+					$(VPATH)/liquidRibObj.$(OBJEXT) \
+					$(VPATH)/liquidRibNode.$(OBJEXT) \
+					$(VPATH)/liquidRibData.$(OBJEXT) \
+					$(VPATH)/liquidGlobalHelpers.$(OBJEXT) \
+					$(VPATH)/liquidRibSurfaceData.$(OBJEXT) \
+					$(VPATH)/liquidRibNuCurveData.$(OBJEXT) \
+					$(VPATH)/liquidRibMeshData.$(OBJEXT) \
+					$(VPATH)/liquidRibLocatorData.$(OBJEXT) \
+					$(VPATH)/liquidRibCoordData.$(OBJEXT) \
+					$(VPATH)/liquidRibLightData.$(OBJEXT) \
+					$(VPATH)/liquidRibHT.$(OBJEXT) \
+					$(VPATH)/liquidRibTranslator.$(OBJEXT) \
+					$(VPATH)/liquidGetSloInfo.$(OBJEXT) \
+					$(VPATH)/liquidGetAttr.$(OBJEXT) \
+					$(VPATH)/liquidAttachPrefAttribute.$(OBJEXT) \
+					$(VPATH)/liquidRibGenData.$(OBJEXT) \
+					$(VPATH)/liqPreviewShader.$(OBJEXT) \
+					$(VPATH)/liquidRibSubdivisionData.$(OBJEXT) \
+					$(VPATH)/liquidMemory.$(OBJEXT) \
+					$(VPATH)/liquidProcessLauncher.$(OBJEXT) \
+					$(VPATH)/liqWriteArchive.$(OBJEXT)
 
 .SUFFIXES: .cpp .$(OBJEXT) .$(PLUGSUF) .c
 
 LIQUIDPLUG = liquid.$(PLUGSUF)
-LIQUIDBIN = liquid
-LIQUIDLIB = libliquid.a
+LIQUIDBIN  = liquid
+LIQUIDLIB  = libliquid.a
 
 default: $(VPATH) $(LIQUIDPLUG) $(LIQUIDBIN) lib
 
@@ -106,14 +108,14 @@ liquidPlug : $(LIQUIDPLUG)
 
 $(LIQUIDPLUG) : liquidPlug.$(OBJEXT) $(LIQUIDLIB)
 	@echo $@
-	$(CPP) $(LDFLAGS) -shared -o $(VPATH)/$(LIQUIDPLUG) $(VPATH)/liquidPlug.$(OBJEXT) $(VPATH)/$(LIQUIDLIB) $(LIBS)
+	@$(CPP) $(LDFLAGS) -shared -o $(VPATH)/$(LIQUIDPLUG) $(VPATH)/liquidPlug.$(OBJEXT) $(VPATH)/$(LIQUIDLIB) $(LIBS)
 	
 $(LIQUIDBIN) : liquidBin.$(OBJEXT) $(LIQUIDLIB)
 	@echo $@
-	$(LD) -DLIQUIDBIN $(LDFLAGS) -o $(VPATH)/$(LIQUIDBIN) $(VPATH)/liquidBin.$(OBJEXT) $(VPATH)/$(LIQUIDLIB) $(LIQUIDBINLIBS)
+	@$(LD) -DLIQUIDBIN $(LDFLAGS) -o $(VPATH)/$(LIQUIDBIN) $(VPATH)/liquidBin.$(OBJEXT) $(VPATH)/$(LIQUIDLIB) $(LIQUIDBINLIBS)
 
 $(LIQUIDLIB) : $(LIQUIDMAINOBJS)
-	$(AR) $(VPATH)/$(LIQUIDLIB) $(LIQUIDOUTMAINOBJS)
+	@$(AR) $(VPATH)/$(LIQUIDLIB) $(LIQUIDOUTMAINOBJS)
 
 $(LIQUIDMAINOBJS) : ../include/liquid.h
 
