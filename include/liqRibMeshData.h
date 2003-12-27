3165c35b59a6ddb36fb78f0d0949dff0594acaf2
scruby <scruby@0aca7f79-8821-0410-92b0-ef0cb72988e5>
2003-12-27 18:47:41 +0000

    Added support for face-varying normals and UVs for the mesh exporter and face-varying UVs for subdivision meshes.  Also implemented limited support for curve rendering using RiCurve.

--- include/liqRibMeshData.h
+++ include/liqRibMeshData.h
@@ -3,21 +3,21 @@
 ** The contents of this file are subject to the Mozilla Public License Version
 ** 1.1 (the "License"); you may not use this file except in compliance with
 ** the License. You may obtain a copy of the License at
-** http://www.mozilla.org/MPL/ 
-** 
+** http://www.mozilla.org/MPL/
+**
 ** Software distributed under the License is distributed on an "AS IS" basis,
 ** WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
 ** for the specific language governing rights and limitations under the
-** License. 
+** License.
 **
-** The Original Code is the Liquid Rendering Toolkit. 
-** 
+** The Original Code is the Liquid Rendering Toolkit.
+**
 ** The Initial Developer of the Original Code is Colin Doncaster. Portions
-** created by Colin Doncaster are Copyright (C) 2002. All Rights Reserved. 
-** 
-** Contributor(s): Berj Bannayan. 
+** created by Colin Doncaster are Copyright (C) 2002. All Rights Reserved.
 **
-** 
+** Contributor(s): Berj Bannayan.
+**
+**
 ** The RenderMan (R) Interface Procedures and Protocol are:
 ** Copyright 1988, 1989, Pixar
 ** All Rights Reserved
@@ -31,7 +31,7 @@
 #define liqRibMeshData_H
 
 /* ______________________________________________________________________
-** 
+**
 ** Liquid Rib Mesh Data Header File
 ** ______________________________________________________________________
 */
@@ -40,23 +40,23 @@
 
 class liqRibMeshData : public liqRibData {
 public: // Methods
-    
+
             liqRibMeshData( MObject mesh );
     virtual ~liqRibMeshData();
-        
+
     virtual void       write();
     virtual bool       compare( const liqRibData & other ) const;
     virtual ObjectType type() const;
-    
+
 private: // Data
-	RtInt     npolys;
+	RtInt     numFaces;
+	RtInt     numPoints;
+	RtInt     numNormals;
 	RtInt   * nverts;
 	RtInt   * verts;
 	const RtFloat * vertexParam;
 	const RtFloat * normalParam;
-    
-    unsigned  totalNumOfVertices;
-    unsigned  textureindex;
+
     bool    	areaLight;
     MString 	name;
     RtMatrix	transformationMatrix;
